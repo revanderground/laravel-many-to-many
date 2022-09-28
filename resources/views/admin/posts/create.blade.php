f502d39769f78faa7b0dd8a0e647270254d3d403
vtnboolean <vnthuanx@gmail.com>
2022-09-28 08:19:40 +0200

    filesystems.php, symlink and enctype

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-12">
 
-            <form action="{{ route('admin.posts.store') }}" method="post">
+            <form action="{{ route('admin.posts.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
 
