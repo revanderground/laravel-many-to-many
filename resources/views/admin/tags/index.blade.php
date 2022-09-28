90fdd5627abc2edb85a2b257ae15b343aa1538bb
vtnboolean <vnthuanx@gmail.com>
2022-09-28 05:43:24 +0200

    tags blades created and edited

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            @if (session('success'))
+                <div class="alert alert-success">
+                    <p>
+                        {{ session('success') }}
+                    </p>
+                </div>
+            @endif
+            @if (session('deleted'))
+            <div class="alert alert-success">
+                <p>
+                    {{ session('deleted') }}
+                </p>
+            </div>
+            @endif
+            <table class="table table-dark table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">
+                        <a href="{{ route('admin.categories.create')}}"
+                            class="btn btn-sm btn-outline-primary">
+                        Create new Tag</a>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @forelse ($tags as $tag)
+
+                        <tr>
+                            <td scope="row">
+                                {{ $category->id }}
+                            </td>
+
+                            <td>
+                                {{ $category->name }}
+                            </td>
+
+                            <td>
+                                <a href="{{ route('admin.tags.show', $tag->id) }}" class="btn btn-sm btn-primary">
+                                    View
+                                </a>
+                                <a href="{{ route('admin.tags.edit', $tag->id) }}" class="btn btn-sm btn-success">
+                                    Edit
+                                </a>
+                                <form class="d-inline" action="{{ route('admin.tags.destroy', $tag->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+
+                        </tr>
+
+                    @empty
+
+                    @endforelse
+
+
+                </tbody>
+              </table>
+
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
---
+++ resources/views/admin/tags/show.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="mx-auto">
+
+            <div class="card my-card ">
+                <div class="card-body">
+
+                    <div class="card-title text-center">
+                        <h4 class="py-2">
+                            Category:
+                        </h4>
+                        <h2>
+                            {{ $tag->name }}
+                        </h2>
+                    </div>
+                </div>
+            </div>
+                <div>
+                        @forelse ( $tag->posts as $post)
+
+                        <div class="card my-card  col-6 mx-auto my-5">
+                            <div class="card-body">
+
+                                <div class="card-title text-center">
+                                    <h2>{{ $post->title }}</h2>
+                                </div>
+
+                                <div class="card-image text-center my-3">
+                                    <img class = "w-50" src="{{ $post->post_image }}" alt="post-image">
+                                </div>
+
+                                <div>
+                                    Category:
+                                        <span class="badge badge-pill badge-info"
+                                        @if (isset($post->category))
+                                            style="color:white; background-color:{{ $post->category->color }}">
+                                            {{ $post->category->name }}
+                                        @else
+                                            style="background-color: lightgrey">
+                                            -
+                                        @endif
+
+                                    </span>
+                                </div>
+
+                                <div class="card-subtitle fs-6 my-3">
+                                    Post with id: {{ $post->id }} | Post written on the: {{ $post->post_date }}
+                                    by {{ $post->user->name }}
+
+                                </div>
+
+                                <div>
+                                    {{ $post->user->name }} has the roles of:
+                                    @forelse ( $post->user->roles as $role)
+                                        {{ $role->name }}
+
+                                    @empty
+                                        No roles.
+                                    @endforelse
+                                </div>
+
+                                <div class="card-text my-3">
+                                    {{ $post->post_content }}
+                                </div>
+
+                             </div>
+                        </div>
+                        @empty
+                            <li>This category has no posts</li>
+                        @endforelse
+
+                </div>
+
+
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
